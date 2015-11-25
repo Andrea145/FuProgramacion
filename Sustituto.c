f480fc11560896436cc687828f3a820efe98365d
Andrea145 <andi.c.flores@gmail.com>
2015-11-24 20:10:49 -0600

    Cadena

---
+++ Sustituto.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+char cadena[20];
+char cambiar;
+char sustituir;
+int i;
+ int main(){
+printf("Escriba una palabra\n "); 
+scanf("%s",cadena);
+printf("Caracter para cambiar"); 
+scanf("%s",cambiar);
+printf("Ingrese el caracter que desea sustituir");
+scanf("%s",sustituir);
+ 
+for (i=0;i<0;i++){ 
+    if (cadena[i]==cambiar){ 
+       cadena[i]==sustituir; 
+       }
+       }
+ 
+    printf("%s",cadena); 
+   
+ 
+}
